06fb6a3c956555cd2030f2d18cef3644dac75a39
Serge Starinets <sergestarynets@Serges-MacBook-Pro-M1.local>
2022-07-20 20:09:53 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import TopBar from './components/TopBar/TopBar';
+
+function App() {
+
+  return (
+    <>
+    <CssBaseline />
+    <div className="App">
+      <TopBar />
+    </div>
+    </>
+  );
+}
+
+export default App;
